c02a20eb83ef477a061b95f80d0c1061b7e16f4a
Andrew Holway <andrew.holway@brickblock.io>
2018-11-06 21:06:39 +0100

    now with if

--- service.py
+++ service.py
@@ -9,12 +9,15 @@ CORS(app, resources='*')
 
 @app.route('/')
 def hello_world():
-    url = "http://" + os.environ['UPSTREAM_SERVICE'] + "." + os.environ['BRANCH'] + "-" + os.environ['UPSTREAM_SERVICE'] + ":" + os.environ['UPSTREAM_PORT']
-    request = requests.get(url, timeout=0.5)
-    chain = json.loads(request.text)
-    print(type(chain))
     branch = dict()
-    branch["microservice-beta"] = os.environ['BRANCH']
+    if 'UPSTREAM_SERVICE' in os.environ:
+        url = "http://" + os.environ['UPSTREAM_SERVICE'] + "." + os.environ['BRANCH'] + "-" + os.environ['UPSTREAM_SERVICE'] + ":" + os.environ['UPSTREAM_PORT']
+        request = requests.get(url, timeout=0.5)
+        chain = json.loads(request.text)
+    else
+        chain = []
+
+    branch[os.environ['THIS_SERVICE']] = os.environ['BRANCH']
     chain.append(branch)
     return json.dumps(chain)
 
@@ -24,3 +27,4 @@ def healthz():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='8080')
+
